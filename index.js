5a3f0fecb95a75c6cf0db1304c673d6aab006564
Staicy Stadtler <staicy@hotmail.de>
2023-08-17 11:00:33 +0200

    js-objects introduction

---
+++ index.js
@@ -0,0 +1,42 @@
+// const person = {
+//   name: "Till",
+//   age: "30",
+//   job: "developer",
+// };
+// console.log(person);
+// console.log(person.name);
+// console.log(person["name"]);
+
+// const person2 = {
+//   1: "test",
+//   "Richard Kurz": "noch ein test",
+// };
+
+// console.log(person2["Richard Kurz"]);
+
+const sister = {
+  name: "Scarlet",
+  age: 32,
+  job: "scientist",
+  address: {
+    street: "Tokyo michi 30",
+    state: "Tokyo",
+    zipCode: "12345",
+  },
+};
+
+console.log(sister.address.street);
+console.log(sister["address"]["zipCode"]);
+
+const person = { name: "Till", age: "30", job: "developer" };
+
+const sister2 = {
+  name: "Scarlet",
+  age: 32,
+  job: "scientist",
+  address: {
+    street: "Tokyo michi 30",
+    state: "Tokyo",
+    zipCode: "12345",
+  },
+};
